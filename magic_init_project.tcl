7fdd373a508a502d5a9fa74e16fb53159047bf97
jonoboderbonobo <jonas2.boerschmann@web.de>
2025-03-09 20:38:59 +0100

    corrected Top-level cell name

--- magic_init_project.tcl
+++ magic_init_project.tcl
@@ -8,7 +8,7 @@
 
 # Change the settings below to match your design:
 # ------------------------------------------------
-set TOP_LEVEL_CELL     tt_um_tt10_jonobo_uB_ips.mag
+set TOP_LEVEL_CELL     tt_um_tt10_jonobo_uB_ips
 set TEMPLATE_FILE      tt_analog_1x2.def
 set POWER_STRIPE_WIDTH 2um                 ;# The minimum width is 1.2um
 
